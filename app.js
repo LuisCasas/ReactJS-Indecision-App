0e8621441628fbd6d84af80565dca1dcb1eeb07d
Luis <sc.luis@gmail.com>
2018-09-29 22:19:10 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-// console.log('test');
-
 // JSX - JavaScript XML
+var template = <p>This is JSX from app.js !!</p>;
+var appRoot = document.getElementById('app');
+
+
+ReactDOM.render(template, appRoot);
