9607dcdc14c958ed255ad6b3993b237cd601b511
Luis <sc.luis@gmail.com>
2018-09-30 17:43:48 +0100

    More JSX logical operators

--- app.js
+++ app.js
@@ -1,28 +1,53 @@
 
 var app = {
     title: 'JSX Web App',
-    content: 'This is a small demo using JSX template'
+    content: 'This is a small demo using JSX template',
+    options: ['One', 'Two']
 };
 
 // JSX - JavaScript XML
 var template = (
     <div>
         <h1>{app.title}</h1>
-        <p>{app.content}</p>
+        {app.content && <p>{app.content}</p>}
+        {getOptions(app.options)}
     </div>
 );
 
+function getOptions(options){
+
+    if(options && options.length > 0){
+        return (
+            <div>
+                <p>These are your options</p>
+                <ul>
+                    <li>Option {options[0]}</li>
+                    <li>Option {options[1]}</li>
+                </ul>
+            </div>
+        );
+    }
+
+    return <p>There are no options</p>;
+}
+
+function getLocation(city){
+    if(city){
+        return <p>Location: {city}</p>;
+    }
+}
+
 var user = {
  name: 'Luis Casas',
- age: '?',
+ age: 21,
  city: 'Edinburgh'
 };
 
 var templateTwo = (
     <div>
-        <h1>{user.name}</h1>
-        <p>Age: {user.age}</p>
-        <p>Location: {user.city}</p>
+        <h1>{user.name ? user.name : 'Unknown'}</h1>
+        {user.age >= 18 && <p>Age: {user.age} </p>}
+        {getLocation(user.city)}
     </div>
 );
 
