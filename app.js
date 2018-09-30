7e0b5d43045735cb0067687953a89da221a06d2b
Luis <sc.luis@gmail.com>
2018-09-30 16:01:46 +0100

    Playing with JSX templates

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 // JSX - JavaScript XML
-var template = <p>This is JSX from app.js !!</p>;
+var template = (
+    <div>
+        <h1>This is JSX from app.js !!</h1>
+        <p>Some text</p>
+    </div>
+);
+
+var templateTwo = (
+    <div>
+        <h1>Luis Casas</h1>
+        <p>Age: none of your business</p>
+        <p>Location: Edinburgh</p>
+    </div>
+);
+
 var appRoot = document.getElementById('app');
 
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
