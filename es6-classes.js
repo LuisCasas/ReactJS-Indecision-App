b19c83a7a8e5ac073d93306672e2d9803148bedd
Luis <sc.luis@gmail.com>
2018-10-04 22:01:34 +0100

    Classes part 2

--- es6-classes.js
+++ es6-classes.js
@@ -13,17 +13,70 @@ class Person{
     }
 
     getDescription(){
-        return `${this.name} is ${this.age}`;
+        return `${this.name} is ${this.age} years old`;
     }
 }
 
-const user1 = new Person('John Garcia', 35);
+class Student extends Person{
 
+    constructor(name, age, degree){
+        super(name, age);
+        this.degree = degree;
+    }
+
+    hasDgree(){
+        return !!this.degree;
+    }
+
+    // equivelant of 
+    getDescription(){
+        let description = super.getDescription();
+
+        if(this.hasDgree()){
+            description += ` with degree on ${this.degree}`;
+        }
+
+        return description; //`${this.name} is ${this.age} years old.`;
+    }    
+}
+
+class Traveller extends Person{
+
+    constructor(name, age, city){
+        super(name, age);
+        this.city = city;
+    }
+
+    getGreeting(){
+        let greetings = super.getGreeting();
+
+        if(this.city){
+            greetings += ` From the city of ${this.city}`;
+        }
+
+        return greetings;
+    }
+}
+
+const traveller1 = new Traveller('Mike tyson', 43, 'New York');
+console.log(traveller1.getGreeting());
+
+const traveller2 = new Traveller('John', 23);
+console.log(traveller2.getGreeting());
+
+/*
+const user1 = new Student('John Garcia', 35, 'Computer Science');
 console.log(user1);
 console.log(user1.getGreeting());
 console.log(user1.getDescription());
+console.log(user1.hasDgree());
+*/
 
-const user2 = new Person();
+/*
+const user2 = new Student();
 console.log(user2);
 console.log(user2.getGreeting());
 console.log(user2.getDescription());
+*/
+
+
