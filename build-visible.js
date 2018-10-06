487fbee146b5cd7b7b4d46aea7cde3e4541e5656
Luis <sc.luis@gmail.com>
2018-10-06 15:14:02 +0100

    Refactoring Visible app using State

--- build-visible.js
+++ build-visible.js
@@ -1,30 +1,61 @@
+
+const randomText = 'This is some ramdon text';
+
+class VisibilityToggle extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.onToggleAction = this.onToggleAction.bind(this);
+
+        this.state = {
+            visible: false
+        };
+    }
+
+    onToggleAction(){
+        this.setState((prevState) => {
+            return {
+                visible: prevState.visible = !prevState.visible
+            };
+        });
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Visibility Toggle</h1>
+                <button onClick={this.onToggleAction}>{this.state.visible ?  'Hide text' : 'Show text'}</button>
+                {(this.state.visible) && <p>{randomText}</p>}
+            </div>       
+        );
+    }
+}
+
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
+
+
+/*
 const appRoot = document.getElementById('app');
 
 const randomText = 'This is some ramdon text';
 
-let buttonAction = 'Show me the hidden text';
-let trueFalse = false;
+let visible = false;
 
 const onToggleAction = () => {
 
-    if(trueFalse){
-        trueFalse = false;
-        buttonAction = 'Show me the hidden text'
-    } else {
-        trueFalse = true;
-        buttonAction = 'Hide the text below';
-    }
+    // visible ? visible = false :  visible = true;
+    visible = !visible;
 
     render();
 };
 
 const render = () => {
-
     const template = (
         <div>
             <h1>Visibility Toggle</h1>
-            <button onClick={onToggleAction}>{buttonAction}</button>
-            {(trueFalse) && <p>{randomText}</p>}
+            <button onClick={onToggleAction}>{visible ? 'Show text' :  'Hide text'}</button>
+            {(visible) && <p>{randomText}</p>}
         </div>
     );
 
@@ -32,3 +63,4 @@ const render = () => {
 }
 
 render();
+*/
