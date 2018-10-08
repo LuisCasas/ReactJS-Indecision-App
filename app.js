41fc42729a90738187108782e686b08eff6e0490
Luis <sc.luis@gmail.com>
2018-10-08 22:15:34 +0100

    Adding default prop values

--- app.js
+++ app.js
@@ -5,21 +5,6 @@
     live-server public
 */
 
-
-/*
-const obj = {
-    name: 'John',
-    getName(){
-        return this.name;
-    }
-}
-
-const getName = obj.getName.bind(obj);
-
-console.log(getName());
-
-*/
-
 class IndecisionApp extends React.Component{
 
     constructor(props){
@@ -28,7 +13,7 @@ class IndecisionApp extends React.Component{
         this.handlePick = this.handlePick.bind(this);
         this.handleAddOption = this.handleAddOption.bind(this);
         this.state = {
-            options: []
+            options: props.options
         }
     }
 
@@ -62,13 +47,11 @@ class IndecisionApp extends React.Component{
     }
 
     render(){
-
-        const title = 'Indecision App!';
         const subtitle = 'Put your life in the hands of a computer';
     
         return(
             <div>
-                <Header title={title} subtitle={subtitle} />
+                <Header subtitle={subtitle} />
                 <Action 
                     hasOptions={this.state.options.length > 0} 
                     handlePick={this.handlePick}
@@ -85,27 +68,22 @@ class IndecisionApp extends React.Component{
     }
 }
 
+IndecisionApp.defaultProps = {
+    options: []
+};
+
 const Header = (props) => {
     return (
         <div>
             <h1>{props.title}</h1>
-            <h2>{props.subtitle}</h2>
+            { props.subtitle && <h2>{props.subtitle}</h2>}
         </div>
     );
 }
 
-/*
-class Header extends React.Component {
-    render(){
-        return (
-            <div>
-                <h1>{this.props.title}</h1>
-                <h2>{this.props.subtitle}</h2>
-            </div>
-        );
-    }
-}
-*/
+Header.defaultProps = {
+    title: 'Indecision App!'
+};
 
 const Action = (props) => {
     return (
@@ -120,24 +98,6 @@ const Action = (props) => {
     );
 };
 
-/*
-class Action extends React.Component{
-    render(){
-        return (
-            <div>
-                <button 
-                    onClick={this.props.handlePick}
-                    disabled={!this.props.hasOptions}
-                    handledeleteoptions={this.props.handleDeleteOptions}
-                    >
-                    What should I do?
-                </button>
-            </div>
-        );
-    }
-}
-*/
-
 const Options = (props) => {
     return(
         <div>
@@ -145,50 +105,17 @@ const Options = (props) => {
             <p>Your options here:</p>
             <p>Number of options: {props.options.length}</p>
             <ul>
-                {
-                   // this.props.options.map((option) => <li key={option}>{option}</li>)
-                    props.options.map((option) => <Option key={option} optionText={option} />)
-                }
+                {props.options.map((option) => <Option key={option} optionText={option} />)}
             </ul>
         </div>
     );
 };
-
-/*
-class Options extends React.Component{
-    render(){
-        return(
-            <div>
-                <p><button onClick={this.props.handleDeleteOptions}>Remove All</button> </p>
-                <p>Your options here:</p>
-                <p>Number of options: {this.props.options.length}</p>
-                <ul>
-                    {
-                       // this.props.options.map((option) => <li key={option}>{option}</li>)
-                        this.props.options.map((option) => <Option key={option} optionText={option} />)
-                    }
-                </ul>
-            </div>
-        );
-    }
-}
-*/
 
 const Option = (props) => {
     return(
         <li>{props.optionText}</li>
     );
 };
-
-/*
-class Option extends React.Component{
-    render(){
-        return(
-            <li>{this.props.optionText}</li>
-        );
-    }
-}
-*/
 
 class AddOption extends React.Component{
 
@@ -225,19 +152,3 @@ class AddOption extends React.Component{
 }
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
-
-/*
-const User = (props) => {
-
-    return (
-        <div>
-            <p>User: {props.name}</p>
-            <p>Age: {props.age}</p>
-        </div>
-
-    );
-};
-
-
-ReactDOM.render(<User name="John" age={33} />, document.getElementById('app'));
-*/
