3b3ce8f1b2a9f8320edc1dc399070b3552b8d68a
Luis <sc.luis@gmail.com>
2018-10-13 14:19:21 +0100

    Refactoring ES6 class properties

--- app.js
+++ app.js
@@ -4,3 +4,29 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+
+class OldSyntax{
+    constructor(){
+        this.name = 'John';
+        this.getGreeting = this.getGreeting.bind(this);
+    }
+
+    getGreeting(){
+        return 'Hello '+this.name;
+    }
+}
+
+const oldSyntax = new OldSyntax();
+const getGreeting = oldSyntax.getGreeting;
+console.log(getGreeting());
+
+class NewSyntax{
+    name = 'Mike';
+    getGreeting = () => {
+        return 'Hello!! '+this.name;
+    }
+}
+const newSyntax = new NewSyntax();
+const newGreeting = newSyntax.getGreeting;
+console.log(newGreeting());
