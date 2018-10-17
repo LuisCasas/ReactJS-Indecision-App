21469b95e8bf44e4b84f9ba50872c41e0979c325
Luis <sc.luis@gmail.com>
2018-10-17 21:21:31 +0100

    Adding style to the buttons

--- Options.js
+++ Options.js
@@ -2,22 +2,27 @@ import React from 'react';
 import Option from './Option';
 
 const Options = (props) => (
-        <div>
-            <p><button onClick={props.handleDeleteOptions}>Remove All</button> </p>
-            <p>Number of options: {props.options.length}</p>
-            {props.options.length === 0 && <p>Please add an option</p>}
-            <ul>
-                {
-                    props.options.map((option) => (
-                    <Option 
-                        key={option} 
-                        optionText={option} 
-                        handleAddOption={props.handleDeleteOption}
-                    />
-                    ))
-                }
-            </ul>
-        </div>
+    <div>
+        <button 
+            className="button button--link"
+            onClick={props.handleDeleteOptions}
+        >
+            Remove All
+        </button> 
+        <p>Number of options: {props.options.length}</p>
+        {props.options.length === 0 && <p>Please add an option</p>}
+        <ul>
+            {
+                props.options.map((option) => (
+                <Option 
+                    key={option} 
+                    optionText={option} 
+                    handleAddOption={props.handleDeleteOption}
+                />
+                ))
+            }
+        </ul>
+    </div>
 );
 
 
