b72bc4918842120c344a61768d84ab30ed409440
Luis <sc.luis@gmail.com>
2018-10-19 21:56:54 +0100

    Adding style to the modal window

--- OptionModal.js
+++ OptionModal.js
@@ -6,10 +6,12 @@ const OptionModal = (props) =>(
             ariaHideApp={false}
             isOpen={!!props.selectedOption}
             contentLabel="Selected Option"
+            closeTimeoutMS={200}
+            className="modal"
         >
-            <h3>Selected Option</h3>
-            {props.selectedOption && <p>{props.selectedOption}</p>}
-            <button onClick={props.handleClearOption}>Close</button>
+            <h3 className="modal__title">Selected Option</h3>
+            {props.selectedOption && <p class="modal__body">{props.selectedOption}</p>}
+            <button className="button" onClick={props.handleClearOption}>Close</button>
         </Modal>
 );
 
